544c67e817a62627d686dd58390bcac308573613
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-22 16:26:48 +0300

    First Commit

---
+++ 7.C sharp Using Constant/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _7.C_sharp_Using_Constant
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnFindPerimeter = new System.Windows.Forms.Button();
+			this.txtSideLength = new System.Windows.Forms.TextBox();
+			this.lblSideLength = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.lblPerimeter = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnFindPerimeter
+			// 
+			this.btnFindPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnFindPerimeter.Location = new System.Drawing.Point(12, 105);
+			this.btnFindPerimeter.Name = "btnFindPerimeter";
+			this.btnFindPerimeter.Size = new System.Drawing.Size(222, 97);
+			this.btnFindPerimeter.TabIndex = 5;
+			this.btnFindPerimeter.Text = "FIND";
+			this.btnFindPerimeter.UseVisualStyleBackColor = true;
+			this.btnFindPerimeter.Click += new System.EventHandler(this.btnFindPerimeter_Click);
+			// 
+			// txtSideLength
+			// 
+			this.txtSideLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtSideLength.Location = new System.Drawing.Point(12, 12);
+			this.txtSideLength.Name = "txtSideLength";
+			this.txtSideLength.Size = new System.Drawing.Size(222, 67);
+			this.txtSideLength.TabIndex = 4;
+			this.txtSideLength.TextChanged += new System.EventHandler(this.txtSideLength_TextChanged);
+			// 
+			// lblSideLength
+			// 
+			this.lblSideLength.AutoSize = true;
+			this.lblSideLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSideLength.Location = new System.Drawing.Point(548, 237);
+			this.lblSideLength.Name = "lblSideLength";
+			this.lblSideLength.Size = new System.Drawing.Size(0, 61);
+			this.lblSideLength.TabIndex = 7;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(575, 50);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(606, 479);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// lblPerimeter
+			// 
+			this.lblPerimeter.AutoSize = true;
+			this.lblPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPerimeter.Location = new System.Drawing.Point(30, 427);
+			this.lblPerimeter.Name = "lblPerimeter";
+			this.lblPerimeter.Size = new System.Drawing.Size(0, 61);
+			this.lblPerimeter.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1236, 569);
+			this.Controls.Add(this.lblPerimeter);
+			this.Controls.Add(this.lblSideLength);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.btnFindPerimeter);
+			this.Controls.Add(this.txtSideLength);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnFindPerimeter;
+		private System.Windows.Forms.TextBox txtSideLength;
+		private System.Windows.Forms.Label lblSideLength;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label lblPerimeter;
+	}
+}
+
---
+++ 7.C sharp Using Constant/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7.C_sharp_Using_Constant
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnFindPerimeter_Click(object sender, EventArgs e)
+		{
+			//reads the value from the box and saves it, after conversion, to sideLength
+			double sideLength = double.Parse(txtSideLength.Text);
+			//every square has exactly 4 sides
+			//const keyword indicates number of sides should not change
+			const ushort numberOfSides = 4;
+			//4*10=40 perimeter when each side is 10
+			//4*20=80 perimeter when each side is 20
+			double perimeter = numberOfSides * sideLength;
+			//line below, at run time, will show the perimeter
+			lblPerimeter.Text = $"Perimeter={perimeter}";
+		}
+
+		private void txtSideLength_TextChanged(object sender, EventArgs e)
+		{
+			//whenever the text is changed in the box,
+			//the label next to the square is updated with that value
+			lblSideLength.Text = txtSideLength.Text;
+		}
+	}
+}
